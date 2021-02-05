966c1bae2c3aa83a09e7a74d46b5949c2af8f30d
gretkierewicz <piotr.gretkierewicz@gmail.com>
2021-02-05 16:33:41 +0100

    Checked max len for Cars model's fields - updated

--- cars/models.py
+++ cars/models.py
@@ -5,5 +5,5 @@ class Cars(models.Model):
     class Meta:
         unique_together = ['car_make', 'model_name']
 
-    car_make = models.CharField(max_length=64)
-    model_name = models.CharField(max_length=64)
+    car_make = models.CharField(max_length=72)
+    model_name = models.CharField(max_length=136)
--- utils/nhtsa_requests.py
+++ utils/nhtsa_requests.py
@@ -1,6 +1,32 @@
 import requests
 
 
+def get_make_name_max_len():
+    """
+    Function to check max len of car's make name
+    result = 72 (05 FEB 2021)
+    """
+    response = requests.get('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json')
+    return max([len(_.get('Make_Name')) for _ in response.json().get('Results')])
+
+
+def get_model_name_max_len():
+    """
+    HEAVY! Function to check max len of car's model name
+    result = 136 (05 FEB 2021)
+    """
+    max_len = 0
+    list_response = requests.get('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json')
+    for make in [_.get('Make_Name') for _ in list_response.json().get('Results')]:
+        response = requests.get(f'https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/{make}?format=json')
+        try:
+            _ = max([len(_.get('Model_Name')) for _ in response.json().get('Results')])
+            max_len = _ if _ > max_len else max_len
+        except:
+            continue
+    return max_len
+
+
 class CarsModelsForMake:
     """
     Simple class pulling data from https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/{make} endpoint
