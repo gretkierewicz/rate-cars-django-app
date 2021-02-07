d61c2199f3bdbbbb40a25d70fcadc1c0bd2e63d2
gretkierewicz <piotr.gretkierewicz@gmail.com>
2021-02-07 21:04:32 +0100

    Tests rewritten

--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,4 @@
+import json
 import random
 import string
 
@@ -25,10 +26,6 @@ def get_urls_for_model(make, model):
     }
 
 
-def get_post_data_for_car(make, model):
-    return {'make': make, 'model_name': model}
-
-
 class CarsTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
@@ -41,13 +38,6 @@ class CarsTests(APITestCase):
         cls.instance = Cars.objects.create(make='HONDA')
         cls.instance.models.create(name='Civic')
 
-        cls.urls = {
-            'cars-list': "/cars/",
-            'cars-popular': f"/cars/popular/",
-            **get_urls_for_make(make=cls.instance.make),
-            **get_urls_for_model(make=cls.instance.make, model=cls.instance.models.first().name),
-        }
-
         models_list = {
             'HONDA': ['Insight', 'Pilot', 'Ridgeline', ],
             'FORD': ['Thunderbird', 'Edge', 'Turus', ],
@@ -57,80 +47,106 @@ class CarsTests(APITestCase):
             for model in models:
                 obj.models.create(name=model)
 
-    def test_get_endpoints_status_code(self):
-        for view, url in self.urls.items():
-            with APITestCase.subTest(self, url):
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, status.HTTP_200_OK,
-                                 response.get('data') or response.get('errors') or response)
-
-    def test_post_car_model(self):
-        APITestCase.skipTest(self, 'time-heavy')
-        data = {
-            'HONDA': ['Fit', 'CRF250', 'Sportrax'],
-            'FORD': ['Escape', 'Focus', 'Fairmont'],
+        cls.valid_urls = {
+            'cars-list': "/cars/",
+            'cars-popular': f"/cars/popular/",
+            **get_urls_for_make(make=cls.instance.make),
+            **get_urls_for_model(make=cls.instance.make, model=cls.instance.models.first().name),
         }
-        for make, models in data.items():
-            for model in models:
-                with APITestCase.subTest(self, f"{make} - {model}"):
-                    response = self.client.post(
-                        self.urls['cars-list'],
-                        data=get_post_data_for_car(make=make, model=model)
-                    )
-                    self.assertEqual(response.status_code, status.HTTP_201_CREATED,
-                                     response.get('data') or response.get('errors') or response)
 
-    def test_post_rate(self):
-        for rate in range(1, 5):
-            with APITestCase.subTest(self, rate):
-                response = self.client.post(self.urls['car-models-rate'], data={'rate': rate})
-                self.assertEqual(response.status_code, status.HTTP_200_OK,
-                                 response.get('data') or response.get('errors') or response)
+        cls.valid_car_data = {
+            'make': 'HONDA',
+            'models': [
+                {'name': 'Passport'},
+                {'name': 'Shadow'},
+                {'name': 'Odyssey'},
+            ]
+        }
+
+        cls.valid_rates = range(1, 5)
+
+        cls.invalid_urls = {
+            'invalid make': get_urls_for_make(make=rand_str(k=10)),
+            'invalid make & model': get_urls_for_model(make=rand_str(k=10), model=rand_str(k=10)),
+            'valid make & invalid model': get_urls_for_model(make=cls.instance.make, model=rand_str(k=10)),
+        }
+
+        cls.invalid_car_data = {
+            'make_data': {
+                'empty': {'make': ''},
+                'wrong': {'make': rand_str()},
+                'right': {'make': 'HONDA'},
+            },
+            'model_data': {
+                'empty': {'model_name': ''},
+                'wrong': {'model_name': rand_str()},
+                # right model not matching HONDA car make (FORD's one)
+                'for different make': {'model_name': 'Tempo'},
+            }
+        }
+
+        cls.invalid_rates = {
+            'less than 1': 0,
+            'negative': -2,
+            'float type': 1.5,
+            'more than 5': 6
+        }
+
+    # VALID section
+
+    def test_status_code_get_valid(self):
+        for view, url in self.valid_urls.items():
+            with APITestCase.subTest(self, f"url: {url}"):
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, status.HTTP_200_OK, response.data or response)
+
+    def test_status_code_post_valid_car(self):
+        response = self.client.post(
+            self.valid_urls['cars-list'],
+            data=json.dumps(self.valid_car_data),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data or response)
+
+    def test_status_code_post_valid_rate(self):
+        for rate in self.valid_rates:
+            with APITestCase.subTest(self, f"rate: {rate}"):
+                response = self.client.post(self.valid_urls['car-models-rate'], data={'rate': rate})
+                self.assertEqual(response.status_code, status.HTTP_200_OK, response.data or response)
+
+    def test_status_code_delete_valid_car_make(self):
+        response = self.client.delete(self.valid_urls['cars-detail'])
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT, response.data or response)
+
+    def test_status_code_delete_valid_car_model(self):
+        response = self.client.delete(self.valid_urls['car-models-detail'])
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT, response.data or response)
 
     # INVALID section
 
-    def test_get_invalid(self):
-        test_urls = {
-            'valid make': get_urls_for_make(make=rand_str(k=10)),
-            'invalid make & model': get_urls_for_model(make=rand_str(k=10), model=rand_str(k=10)),
-            'valid make & invalid model': get_urls_for_model(make=self.instance.make, model=rand_str(k=10)),
-        }
-        for msg, urls in test_urls.items():
+    def test_status_code_get_invalid_car(self):
+        for msg, urls in self.invalid_urls.items():
             for view, url in urls.items():
-                with APITestCase.subTest(self, f"{msg} - {url}"):
+                with APITestCase.subTest(self, f"{msg} url: {url}"):
                     response = self.client.get(url)
-                    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
-                                     response.get('data') or response.get('errors') or response)
+                    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.data or response)
 
-    def test_post_invalid_car(self):
-        APITestCase.skipTest(self, 'time-heavy')
-        make_data = {
-            'empty make': {'make': ''},
-            'wrong make': {'make': rand_str()},
-            'right make': {'make': 'HONDA'},
-        }
-        model_data = {
-            'empty model': {'model_name': ''},
-            'wrong model': {'model_name': rand_str()},
-            # right model not matching HONDA car make (FORD's one)
-            'right model': {'model_name': 'Tempo'},
-        }
-        for make_msg, make in make_data.items():
-            for model_msg, model in model_data.items():
-                with APITestCase.subTest(self, f"{make_msg} - {model_msg}"):
-                    response = self.client.post(self.urls['cars-list'], data={**make, **model})
-                    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
-                                     response.get('data') or response.get('errors') or response)
+    def test_status_code_post_invalid_car(self):
+        for make_msg, make in self.invalid_car_data['make_data'].items():
+            for model_msg, model in self.invalid_car_data['model_data'].items():
+                with APITestCase.subTest(self, f"make: {make_msg}; model: {model_msg}"):
+                    response = self.client.post(self.valid_urls['cars-list'], data={**make, **model})
+                    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data or response)
 
-    def test_post_invalid_rate(self):
-        test_data = {
-            'less than 1': 0,
-            'negative': -2,
-            'float': 1.5,
-            'more than 5': 6
-        }
-        for msg, rate in test_data.items():
-            with APITestCase.subTest(self, msg):
-                response = self.client.post(self.urls['car-models-rate'], data={'rate': rate})
-                self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
-                                 response.get('data') or response.get('errors') or response)
+    def test_status_code_post_invalid_rate(self):
+        for msg, rate in self.invalid_rates.items():
+            with APITestCase.subTest(self, f"rate: {msg}"):
+                response = self.client.post(self.valid_urls['car-models-rate'], data={'rate': rate})
+                self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data or response)
+
+    def test_status_code_delete_invalid_car(self):
+        for msg, urls in self.invalid_urls.items():
+            for view, url in urls.items():
+                with APITestCase.subTest(self, f"{msg} url: {url}"):
+                    response = self.client.delete(self.valid_urls['cars-detail'])
+                    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.data or response)
